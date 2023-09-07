ebc5154d2c051855f78849d5c41aba1ed0be3ea5
Daria Stanilevici <dstanilevichi@gmail.com>
2023-09-07 22:29:12 +0100

    Error fixed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
-import '../Login.css';A
+import '../Login.css';
 
 function Login() {
   const navigate = useNavigate();

b25671d66d444d2f0ac0271da25ded26c51386b5
Daria Stanilevici <dstanilevichi@gmail.com>
2023-09-07 22:28:37 +0100

    Added styles

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
-import '../Login.css';
+import '../Login.css';A
 
 function Login() {
   const navigate = useNavigate();
